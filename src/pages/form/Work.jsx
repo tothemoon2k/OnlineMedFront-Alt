c69d54c7541654ee3d9b00539a3724091871a75a
Justin Garner <justin2013pdx@gmail.com>
2025-01-07 21:05:42 -0500

    Update form link

--- src/pages/form/Work.jsx
+++ src/pages/form/Work.jsx
@@ -4,7 +4,7 @@ import { Widget } from '@typeform/embed-react';
 const Work = () => {
     return (
         <div className='w-screen h-screen flex flex-col justify-center items-center'>
-            <Widget id="RW2PAcY5" style={{ width: '100%', height: '100%' }} className="my-form" />
+            <Widget id="zBS8llmZ" style={{ width: '100%', height: '100%' }} className="my-form" />
         </div>
     );
 };
