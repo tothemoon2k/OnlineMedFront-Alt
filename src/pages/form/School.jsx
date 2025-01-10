a0649abaa3b6e70e29bcc02a34fb06fbc22a1993
Justin Garner <justin2013pdx@gmail.com>
2025-01-10 01:54:14 -0500

    Update school form

--- src/pages/form/School.jsx
+++ src/pages/form/School.jsx
@@ -4,7 +4,7 @@ import { Widget } from '@typeform/embed-react'
 const SchoolForm = () => {
     return (
         <div className='w-screen h-screen flex justify-center items-center'>
-            <Widget id="qHuR6Pme" style={{ width: '100%', height: '100%' }} className="my-form" />
+            <Widget id="SDzEMzNS" style={{ width: '100%', height: '100%' }} className="my-form" />
         </div>
     );
 };
