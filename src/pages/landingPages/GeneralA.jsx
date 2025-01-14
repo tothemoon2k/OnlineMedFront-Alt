9bd9c1de75d0fc7d7d4642193b065b84d737ccfb
Justin Garner <justin2013pdx@gmail.com>
2025-01-14 03:23:35 -0500

    Improve mobile hero

--- src/pages/landingPages/GeneralA.jsx
+++ src/pages/landingPages/GeneralA.jsx
@@ -73,8 +73,8 @@ const GeneralA = () => {
 
                             {/* Testimonials section with more details */}
                             <div className="space-y-6">
-                                <div className="flex items-center space-x-8">
-                                    <div className="flex -space-x-2">
+                                <div className="flex items-center md:space-x-8">
+                                    <div className="-space-x-2 hidden md:flex"> {/* Increased spacing */}
                                         {reviewAvatars.map((i) => (
                                             <img
                                                 key={i}
@@ -97,7 +97,7 @@ const GeneralA = () => {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="text-sm text-gray-500 flex items-center">
+                                <div className="hidden md:flex text-sm text-gray-500 items-center">
                                     <svg className="w-5 h-5 text-green-500 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
                                     </svg>
